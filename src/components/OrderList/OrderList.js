884076034f24adcaad147c9f2c945f36a8afb1b8
Nipa <akternipa941@gmail.com>
2022-03-26 20:53:12 +0600

    update

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const OrderList = (props) => {
     const {cart} = props;
     return (
-        <div className="card border-success mb-3 ms-4">
+        <div className="card border-success mb-3 mx-4">
   <div className="card-header bg-transparent border-success">Order List</div>
   <div className="card-body text-success">
             {
